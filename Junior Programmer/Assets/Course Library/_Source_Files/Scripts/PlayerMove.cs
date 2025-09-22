c464ed5e1dbdc4e49171b9cffcb70dd68f07b8a6
Hayden <02hayden.hershberger@gulllakecs.org>
2025-09-22 08:36:58 -0400

    Step  into the Driver's Seat

--- Junior Programmer/Assets/Course Library/_Source_Files/Scripts/PlayerMove.cs
+++ Junior Programmer/Assets/Course Library/_Source_Files/Scripts/PlayerMove.cs
@@ -10,11 +10,17 @@ public class PlayerMove : MonoBehaviour
 
 
 	public float speed = 20;
+	public float turnSpeed;
+	public float horizontalInput;
+	public float forwardInput;
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-	transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+	forwardInput = Input.GetAxis("Vertical");
 
+	// Move the vehicle forward
+	transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+	transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }

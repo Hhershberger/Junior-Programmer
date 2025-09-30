ff3c83d2d8cadcfea460ea555e21e82ed2bc06ef
HERSHBERGER <02hayden.hershberger@gulllakecs.org>
2025-09-30 11:09:21 -0400

    Game Over

--- Junior Programmer/Assets/Course Library/Scripts/Target.cs
+++ Junior Programmer/Assets/Course Library/Scripts/Target.cs
@@ -37,12 +37,17 @@ public class Target : MonoBehaviour
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
     private void OnMouseDown(){
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position,
-        explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive){
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position,
+            explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
     private void OnTriggerEnter(Collider other){
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) {
+            gameManager.GameOver();
+        }
     }
 }
